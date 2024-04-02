c036a166dfbb90e124b9075747f02ebbc0e3c6d0
Laurence Liang <laurence.w.liang@gmail.com>
2024-04-02 14:35:04 -0400

    Create rocket_no_dva.m

---
+++ rocket_no_dva.m
@@ -0,0 +1,33 @@
+function f = rocket_no_dva(t, y)
+f = zeros(10,1);
+
+% define constants
+F0_eng1 = 1;
+F0_eng2 = 1;
+m_1 = 1;
+m_2 = 1;
+m_3 = 1;
+m_4 = 1;
+m_5 = 1;
+c_1 = 1;
+c_2 = 1;
+c_3 = 1;
+c_4 = 1;
+c_5 = 1;
+k_1 = 1;
+k_2 = 1;
+k_3 = 1;
+k_4 = 1;
+k_5 = 1;
+
+% define equations
+f(1) = 0;
+f(2) = 0;
+f(3) = 0;
+f(4) = 0;
+f(5) = 0;
+f(6) = 0;
+f(7) = 0;
+f(8) = 0;
+f(9) = 0;
+f(10) = 0;
