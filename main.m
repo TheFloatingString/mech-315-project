b3d79d3a6c706ff965f71c9441215dccfc4a22de
Laurence Liang <laurence.w.liang@gmail.com>
2024-04-03 17:20:36 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,15 +1,28 @@
-tspan = [0: .01: 10];
-y0 = [0;0;0;0;0;0];
-[t,y] = ode23('reference_functions', tspan, y0);
-subplot(311);
+tspan = [0: .01: 5];
+% y0 = [0;0;0;0;0;0];
+y0 = [0;0;0;0;0;0;0;0;0;0;0;0]
+[t,y] = ode23('rocket_no_dva', tspan, y0);
+subplot(611);
 plot(t,y(:,1));
 xlabel('t');
 ylabel('x1(t)');
-subplot(312);
+subplot(612);
 plot(t,y(:,3));
 xlabel('t');
 ylabel('x2(t)');
-subplot(313);
+subplot(613);
 plot(t,y(:,5))
 xlabel('t');
 ylabel('x3 (t)');
+subplot(614);
+plot(t,y(:,7))
+xlabel('t');
+ylabel('x4 (t)');
+subplot(615);
+plot(t,y(:,9))
+xlabel('t');
+ylabel('x5 (t)');
+subplot(616);
+plot(t,y(:,11))
+xlabel('t');
+ylabel('x6 (t)');
