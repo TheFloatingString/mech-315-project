7f06941b8a72a00ed8f6f7522337239c3cd6766e
Laurence Liang <laurence.w.liang@gmail.com>
2024-04-07 23:20:11 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,28 +1,39 @@
-tspan = [0: .001: 10];
+tspan = [0: .01: 100];
 % y0 = [0;0;0;0;0;0];
 y0 = [0;0;0;0;0;0;0;0;0;0;0;0];
 [t,y] = ode23('rocket_no_dva', tspan, y0);
-subplot(611);
+subplot(311);
 plot(t,y(:,1));
-xlabel('t');
-ylabel('x1(t)');
-subplot(612);
+title('Engine 1 Displacement')
+xlabel('t (s)');
+ylabel('x1(t) (m)');
+subplot(312);
 plot(t,y(:,3));
-xlabel('t');
-ylabel('x2(t)');
-subplot(613);
+title('Engine 2 Displacement')
+xlabel('t (s)');
+ylabel('x2(t) (m)');
+subplot(313);
 plot(t,y(:,5))
-xlabel('t');
-ylabel('x3 (t)');
-subplot(614);
-plot(t,y(:,7))
-xlabel('t');
-ylabel('x4 (t)');
-subplot(615);
-plot(t,y(:,9))
-xlabel('t');
-ylabel('x5 (t)');
-subplot(616);
-plot(t,y(:,11))
-xlabel('t');
-ylabel('x6 (t)');
+title('Stage 1 Displacement')
+xlabel('t (s)');
+ylabel('x3 (t) (m)');
+% saveas(gcf,'fig.png')
+hold off
+
+% subplot(311);
+% plot(t,y(:,7))
+% title('Stage 1 Rotation for J_0')
+% xlabel('t (s)');
+% ylabel('x4 (t)');
+% subplot(312);
+% plot(t,y(:,9))
+% title('Stage 2 Displacement')
+% xlabel('t (s)');
+% ylabel('x5 (t) (m)');
+% subplot(313);
+% plot(t,y(:,11))
+% title('Payload Displacement')
+% xlabel('t (s)');
+% ylabel('x6 (t) (m)');
+% saveas(gcf,'fig-2.png')
+
